1bd45499ca7c8cbcdebe36087fea15ecbd6029f6
florinrusu96 <rusuflorinandrei96@gmail.com>
2020-11-07 15:45:50 +0200

    Configuration page

--- logisteye/src/app/app-routing.module.ts
+++ logisteye/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { LoginComponent } from './pages/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PackageListComponent } from './pages/package-list/package-list.component';
+import { ConfigurationComponent } from './pages/configuration/configuration.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'map', component: MapComponent },
   { path: 'package-list', component: PackageListComponent },
+  { path: 'configuration', component: ConfigurationComponent}
 ];
 
 @NgModule({
